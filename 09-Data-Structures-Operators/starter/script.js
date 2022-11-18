1524146e859703f334eddb8db302efa243b40d67
vinialeixo <vi_aleixodias@hotmail.com>
2022-11-18 11:43:15 -0300

    coding challenge 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -34,34 +34,135 @@ const restaurant = {
     console.log(`You ingridients are ${ing1}, ${ing2}, ${ing3}`);
   },
 };
-
 ///////////////////////////////////////
-const rest1 = {
-  name: 'Capri',
-  numGuests: 0,
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+
+2. The first player in any player array is the goalkeeper and the others are field players.
+For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, 
+and one array ('fieldPlayers') with all the remaining 10 field players
+
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+*/
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
 };
 
-const rest2 = {
-  name: 'La piazza',
-  owner: 'Giovanni Rossi',
+//1
+const [player1, player2] = game.players;
+// console.log(player1, player2);
+
+/*2.*/
+
+// const [gk, ...fieldPlayers] = game.players;
+// console.log(gk, fieldPlayers);
+
+//3.
+// const allPlayers = [...player1, ...player2];
+// console.log(allPlayers);
+
+//4
+// const players1Final = [...player1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+
+//5
+// const { team1, x: draw, team2 } = { ...game.odds };
+// console.log(team1, draw, team2);
+
+//6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array)
+// and prints each of them to the console, along with the number of goals that were scored in total
+//(number of player names passed in)
+
+const prinGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
 };
+
+prinGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+prinGoals('Davies', 'Muller');
+prinGoals(...game.scored);
+
+//7. The team with the lower odd is more likely to win.
+//Print to the console which team is more likely to win,
+//WITHOUT using an if/else statement or the ternary operator.
+
+/////////
+// const rest1 = {
+//   name: 'Capri',
+//   numGuests: 0,
+// };
+
+// const rest2 = {
+//   name: 'La piazza',
+//   owner: 'Giovanni Rossi',
+// };
 
 //OR assigment operator
 // rest1.numGuests = rest1.numGuests || 10;
 // rest2.numGuests = rest2.numGuests || 10;
-rest1.numGuests ||= 10;
-rest2.numGuests ||= 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
 
-//nullish assigment operator (null or undefined)
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
+// //nullish assigment operator (null or undefined)
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
 
-//AND assigment operator
-rest1.owner &&= 'ANONYMOUS';
-rest2.owner &&= 'ANONYMOUS';
+// //AND assigment operator
+// rest1.owner &&= 'ANONYMOUS';
+// rest2.owner &&= 'ANONYMOUS';
 
-console.log(rest1);
-console.log(rest2);
+// console.log(rest1);
+// console.log(rest2);
 
 //Nullish Operator: bring null and undefined(not 0 or empty)
 
@@ -164,7 +265,7 @@ console.log(rest2);
 // });
 
 //OBJECT DESTRUCTURING
-//escrever o nomes extatos da propriedade para o destructuring
+//escrever o nomes exatos da propriedade para o destructuring
 // const { name, openingHours, categories } = restaurant;
 // console.log(name, openingHours, categories);
 

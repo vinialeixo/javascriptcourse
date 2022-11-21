e8c7f25c5ab2cc29fcf6803a51d3a695b4d1290e
vinialeixo <vi_aleixodias@hotmail.com>
2022-11-21 10:26:59 -0300

    study Object Literals && Array for of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,4 +1,21 @@
 'use strict';
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[5]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -6,20 +23,7 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  openingHours,
 
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -34,6 +38,22 @@ const restaurant = {
     console.log(`You ingridients are ${ing1}, ${ing2}, ${ing3}`);
   },
 };
+/*LOOPING ARRAYS for-of */
+
+// const menu = [...restaurant.mainMenu, ...restaurant.categories];
+//for (const item of menu) console.log(item);
+
+//para sabermos o indice precisamos utilizar o entries
+//sem fazer o destructuring
+// for (const item of menu.entries()) {
+//   console.log(`${item[0] + 1} : ${item[1]}`);
+// }
+
+//podemos fazer destructuring dentro do looping
+// for (const [i, element] of menu.entries()) {
+//   console.log(`${i + 1} : ${element}`);
+// }
+
 ///////////////////////////////////////
 // Coding Challenge #1
 
@@ -57,49 +77,49 @@ and one array ('fieldPlayers') with all the remaining 10 field players
 TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 
 */
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
 //1
-const [player1, player2] = game.players;
+// const [player1, player2] = game.players;
 // console.log(player1, player2);
 
 /*2.*/
@@ -123,14 +143,14 @@ const [player1, player2] = game.players;
 // and prints each of them to the console, along with the number of goals that were scored in total
 //(number of player names passed in)
 
-const prinGoals = function (...players) {
-  console.log(players);
-  console.log(`${players.length} goals were scored`);
-};
+// const prinGoals = function (...players) {
+//   console.log(players);
+//   console.log(`${players.length} goals were scored`);
+// };
 
-prinGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-prinGoals('Davies', 'Muller');
-prinGoals(...game.scored);
+// prinGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// prinGoals('Davies', 'Muller');
+// prinGoals(...game.scored);
 
 //7. The team with the lower odd is more likely to win.
 //Print to the console which team is more likely to win,

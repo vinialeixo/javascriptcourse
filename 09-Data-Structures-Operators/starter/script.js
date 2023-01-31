48db220585ed7ec4dd92983f3f065aef36e9fd2c
vinialeixo <vi_aleixodias@hotmail.com>
2023-01-31 10:24:58 -0300

    Study: Looping: Obj Keys, Values and Entries

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -44,6 +44,84 @@ const restaurant = {
   },
 };
 
+//SET:
+/* SETs: nunca pode ter um conjunto com duplicatadas */
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console,
+ along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console 
+(We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1. Loop over the game.scored array and print each player name to the console,
+//along with the goal number (Example: "Goal 1: Lewandowski")
+
+// for (const [i, name] of game.scored.entries())
+//   console.log(`Goal ${i + 1}: ${name} `);
+
+//   2. Use a loop to calculate the average odd and log it to the console
+// (We already studied how to calculate averages, you can go check if you don't remember)
+
 //The Object.keys() method returns an array of a given object's own enumerable property names,
 //iterated in the same order that a normal loop would.
 
@@ -57,6 +135,7 @@ const restaurant = {
 // }
 // console.log(openStr);
 
+/*PROPERTY VALUERS */
 //The Object.values() method returns an array of a given object's own enumerable property values,
 //in the same order as that provided by a for...in loop.
 
@@ -66,12 +145,12 @@ const restaurant = {
 //The Object.entries() method returns an array of a
 //given object's own enumerable string-keyed property [key, value] pairs.
 
-const entries = Object.entries(openingHours);
-// console.log(entries);
+// const entries = Object.entries(openingHours);
+// // console.log(entries);
 
-for (const [key, { open, close }] of entries) {
-  console.log(`On ${key} we open at ${open} and close at ${close} `);
-}
+// for (const [key, { open, close }] of entries) {
+//   console.log(`On ${key} we open at ${open} and close at ${close} `);
+// }
 
 /////////////////////////////////
 /*Optional Chaining? */
@@ -140,47 +219,6 @@ and one array ('fieldPlayers') with all the remaining 10 field players
 
 TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 
-*/
-// const game = {
-//   team1: 'Bayern Munich',
-//   team2: 'Borrussia Dortmund',
-//   players: [
-//     [
-//       'Neuer',
-//       'Pavard',
-//       'Martinez',
-//       'Alaba',
-//       'Davies',
-//       'Kimmich',
-//       'Goretzka',
-//       'Coman',
-//       'Muller',
-//       'Gnarby',
-//       'Lewandowski',
-//     ],
-//     [
-//       'Burki',
-//       'Schulz',
-//       'Hummels',
-//       'Akanji',
-//       'Hakimi',
-//       'Weigl',
-//       'Witsel',
-//       'Hazard',
-//       'Brandt',
-//       'Sancho',
-//       'Gotze',
-//     ],
-//   ],
-//   score: '4:0',
-//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-//   date: 'Nov 9th, 2037',
-//   odds: {
-//     team1: 1.33,
-//     x: 3.25,
-//     team2: 6.5,
-//   },
-// };
 
 //1
 // const [player1, player2] = game.players;

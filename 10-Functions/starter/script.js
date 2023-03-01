c45c03ba6d1a40e714ed38d499a1d8815b05f797
vinialeixo <vi_aleixodias@hotmail.com>
2023-03-01 10:42:36 -0300

    Studied Callback Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,61 +1,98 @@
 'use strict';
 
-//DEFAULT PARAMETERS
-// const bookings = [];
+// //DEFAULT PARAMETERS
+// // const bookings = [];
 
-// const createBooking = function (
-//   flightNum,
-//   numPassengers = 1,
-//   price = 199 * numPassengers
-// ) {
-//   //ES5
-//   //   numPassengers = numPassengers || 1;
-//   //   price = price || 199;
+// // const createBooking = function (
+// //   flightNum,
+// //   numPassengers = 1,
+// //   price = 199 * numPassengers
+// // ) {
+// //   //ES5
+// //   //   numPassengers = numPassengers || 1;
+// //   //   price = price || 199;
 
-//   const booking = {
-//     flightNum,
-//     numPassengers,
-//     price,
-//   };
+// //   const booking = {
+// //     flightNum,
+// //     numPassengers,
+// //     price,
+// //   };
 
-//   console.log(booking);
-//   bookings.push();
+// //   console.log(booking);
+// //   bookings.push();
+// // };
+
+// // createBooking('LH1994');
+// // createBooking('LH1995', 2, 800);
+// // createBooking('LH1996', 5);
+
+// // createBooking('LH1997', undefined, 100);
+
+// const flight = 'LH1234';
+// const jonas = {
+//   name: 'Vinicius Dias',
+//   passport: 444445324,
 // };
 
-// createBooking('LH1994');
-// createBooking('LH1995', 2, 800);
-// createBooking('LH1996', 5);
+// const checkIn = function (flightNum, passenger) {
+//   // (flightNum = 'LH999'), nao troca o valor da variavel
+//   passenger.name = 'Mr.' + passenger.name;
 
-// createBooking('LH1997', undefined, 100);
+//   if (passenger.passport === 444445324) {
+//     //alert('Check in');
+//   } else {
+//     //alert('Wrong passport!');
+//   }
+// };
 
-const flight = 'LH1234';
-const jonas = {
-  name: 'Vinicius Dias',
-  passport: 444445324,
-};
+// // checkIn(flight, jonas);
+// // console.log(jonas);
 
-const checkIn = function (flightNum, passenger) {
-  // (flightNum = 'LH999'), nao troca o valor da variavel
-  passenger.name = 'Mr.' + passenger.name;
+// /* Dois funcoes para o mesmo objeto pode causar confusao */
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000);
+//   console.log(jonas);
+//   console.log(person.passport);
+// };
 
-  if (passenger.passport === 444445324) {
-    alert('Check in');
-  } else {
-    alert('Wrong passport!');
-  }
-};
-
+// newPassport(jonas);
 // checkIn(flight, jonas);
-// console.log(jonas);
-
-/* Dois funcoes para o mesmo objeto pode causar confusao */
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 10000000);
-  console.log(jonas);
-  console.log(person.passport);
-};
-
-newPassport(jonas);
-checkIn(flight, jonas);
 
 //JS NAO TEM PASSAGEM POR REFERENCIA, APENAS POR VALUE.
+
+/* HIGH-ORDER FUNCTION 
+  - A function that receives another functions as an argument, that returns a new 
+  function, or both
+  -this is only possible because of first-class functions
+*/
+
+//Functions Accepting Callback Functions
+
+const oneWrod = function (str) {
+  return str.replace(/ /g, '-').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//HIGH-ORDER FUNCTION
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed by: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`); //nome da funcao
+};
+
+transformer('JavaScript is the best!', upperFirstWord); //upperFirstWord = callback function
+transformer('JavaScript is the best!', oneWrod);
+
+//JS uses callbacks all the time
+const highFive = function () {
+  console.log('high five');
+};
+
+document.body.addEventListener('click', highFive); //highfive = callback functions
+
+['jonas', 'Martha', 'Adma'].forEach(highFive);
